816c470924e47e7125a7c323519b2cd541c976f3
Preetam-Pati <preetam6teen@gmail.com>
2022-06-17 20:25:35 +0530

    Added login feature, sign up page and sign in page

--- food_recipe_generator.c
+++ food_recipe_generator.c
@@ -20,6 +20,15 @@ typedef struct queue
     struct queue *next;
 } queue;
 
+typedef struct usr
+{
+    char f_name[25];
+    char u_name[25];
+    char e_mail[30];
+    char mob[13];
+    char pwd[25];
+} usr;
+
 queue *enqueue(queue *head, int value)
 {
     if (head == NULL)
@@ -54,11 +63,11 @@ queue *dequeue(queue *head)
     return head;
 }
 
-void init_all(int num, int visi[num], int ad_mat[num][num])
+void init_all(int num, int visited[num], int ad_mat[num][num])
 {
     for (int i = 0; i < num; i++)
     {
-        visi[i] = 0;
+        visited[i] = 0;
     }
 
     for (int i = 0; i < num; i++)
@@ -114,6 +123,7 @@ void cns_graph(int num, node arr[num], int ad_mat[num][num])
         printf("\n\t\tEnter element %d = ", i + 1);
         scanf(" %[^\n]s", &arr[i].ele);
         // fgets(arr[i].ele, 50, stdin);
+         
     }
 
     printf("\n\t\tEnter the number of edge = ");
@@ -147,7 +157,7 @@ void calc_in_deg(int num, int ad_mat[num][num], int in_deg[num])
     }
 }
 
-void topo_sort(int num, node arr[num], int ad_mat[num][num], int visi[num], int in_deg[num], queue *head)
+void topo_sort(int num, node arr[num], int ad_mat[num][num], int visited[num], int in_deg[num], queue *head)
 {
     int pop_ele, time = 0;
     printf("\n");
@@ -162,13 +172,13 @@ void topo_sort(int num, node arr[num], int ad_mat[num][num], int visi[num], int 
 
     while (head != NULL)
     {
-        if (visi[head->val] != 1)
+        if (visited[head->val] != 1)
         {
             if (time == 0)
             {
                 printf("\t\t%s\n", arr[head->val].ele);
                 usleep(1000000);
-                visi[head->val] = 1;
+                visited[head->val] = 1;
                 time = 1;
             }
             else
@@ -176,7 +186,7 @@ void topo_sort(int num, node arr[num], int ad_mat[num][num], int visi[num], int 
                 printf("\n\t\t  %c\n", 25);
                 printf("\t\t%s\n", arr[head->val].ele);
                 usleep(1000000);
-                visi[head->val] = 1;
+                visited[head->val] = 1;
             }
         }
         pop_ele = head->val;
@@ -186,7 +196,7 @@ void topo_sort(int num, node arr[num], int ad_mat[num][num], int visi[num], int 
         {
             if (ad_mat[pop_ele][i] == 1)
             {
-                if (visi[i] != 1 && in_deg[i] != 0)
+                if (visited[i] != 1 && in_deg[i] != 0)
                 {
                     in_deg[i] -= 1;
                     if (in_deg[i] == 0)
@@ -195,7 +205,7 @@ void topo_sort(int num, node arr[num], int ad_mat[num][num], int visi[num], int 
                         printf("\n\t\t  %c\n", 25);
                         printf("\t\t%s\n", arr[i].ele);
                         usleep(1000000);
-                        visi[i] = 1;
+                        visited[i] = 1;
                     }
                 }
             }
@@ -293,11 +303,199 @@ void pwd_gen()
     pwd[i] = '\0';
 }
 
+void login_pg()
+{
+    system("cls");
+    printf("\n\n");
+    system("color 0E");
+    char a = 177, b = 219;
+    printf("\t\t");
+    for (int i = 0; i < 88; i++)
+    {
+        printf("%c", b);
+    }
+    printf("\r");
+    printf("\t\t");
+    printf("\n\t\t\t\t\t\tLOGIN PAGE\n");
+    printf("\t\t");
+    for (int i = 0; i < 88; i++)
+    {
+        printf("%c", b);
+    }
+    printf("\r");
+    printf("\t\t");
+
+    int choice;
+
+    printf("\n\n\t\t1.Sign Up");
+    printf("\n\t\t2.Sign In");
+    printf("\n\t\t3.Exit");
+
+    printf("\n\t\tEnter your choice = ");
+    scanf("%d", &choice);
+
+    while (choice != 3)
+    {
+        if (choice == 1)
+        {
+            sign_up();
+        }
+        else if (choice == 2)
+        {
+            sign_in();
+        }
+
+        printf("\n\t\tEnter your choice = ");
+        scanf("%d", &choice);
+    }
+}
+
+void sign_in()
+{
+    system("cls");
+    printf("\n\n");
+    system("color 0E");
+    char a = 177, b = 219;
+    printf("\t\t");
+    for (int i = 0; i < 88; i++)
+    {
+        printf("%c", b);
+    }
+    printf("\r");
+    printf("\t\t");
+    printf("\n\t\t\t\t\t\tSIGN IN PAGE\n");
+    printf("\t\t");
+    for (int i = 0; i < 88; i++)
+    {
+        printf("%c", b);
+    }
+    printf("\r");
+    printf("\t\t");
+
+    char tmp1[25], tmp2[25];
+
+    printf("\n\n\t\tEnter your username = ");
+    scanf("%s", tmp1);
+    printf("\n\t\tEnter your password = ");
+
+    int i = 0;
+    char ch;
+
+    while ((ch = _getch()) != 13)
+    {
+        if (ch == 8)
+        {
+            if (i > 0)
+            {
+                i--;
+                printf("\b \b");
+            }
+        }
+        tmp2[i] = ch;
+        printf("*");
+        i++;
+    }
+    tmp2[i] = '\0';
+}
+
+void sign_up()
+{
+    usr user;
+    char ch, ch2, c_pwd[25];
+    int i = 0;
+
+    system("cls");
+    printf("\n\n");
+    system("color 0E");
+    char a = 177, b = 219;
+    printf("\t\t");
+    for (int i = 0; i < 88; i++)
+    {
+        printf("%c", b);
+    }
+    printf("\r");
+    printf("\t\t");
+    printf("\n\t\t\t\t\t\tSIGN UP PAGE\n");
+    printf("\t\t");
+    for (int i = 0; i < 88; i++)
+    {
+        printf("%c", b);
+    }
+    printf("\r");
+    printf("\t\t");
+
+    printf("\n\n\t\tEnter your Full name = ");
+    scanf(" %[^\n]s", user.f_name);
+    printf("\n\t\tSet an username = ");
+    scanf(" %s", user.u_name);
+    printf("\n\t\tEnter your mobile number = ");
+    scanf(" %s", user.mob);
+    printf("\n\t\tEnter your email id = ");
+    scanf(" %s", user.e_mail);
+    printf("\n\t\tSet an password = ");
+    while ((ch = _getch()) != 13)
+    {
+        if (ch == 8)
+        {
+            if (i > 0)
+            {
+                i--;
+                printf("\b \b");
+            }
+        }
+        user.pwd[i] = ch;
+        printf("*");
+        i++;
+    }
+    user.pwd[i] = '\0';
+
+    i = 0;
+
+    printf("\n\t\tConfirm your password = ");
+    while ((ch2 = _getch()) != 13)
+    {
+        if (ch2 == 8)
+        {
+            if (i > 0)
+            {
+                i--;
+                printf("\b \b");
+            }
+        }
+        c_pwd[i] = ch2;
+        printf("*");
+        i++;
+    }
+    c_pwd[i] = '\0';
+
+    if (strcmp(c_pwd, user.pwd) != 0)
+    {
+        printf("\n\n\t\tPassword doesn't match");
+        Beep(750, 300);
+    }
+}
+
+void design()
+{
+    printf("\n\n");
+    system("color 0E");
+    char a = 177, b = 219;
+    printf("\t\t");
+    for (int i = 0; i < 88; i++)
+    {
+        printf("%c", b);
+    }
+    printf("\r");
+    printf("\t\t");
+}
+
 int main()
 {
     int num, edge;
 
     printf("\n::::PROGRAM STARTED::::\n\n");
+
+    login_pg();
 
     print_title();
 
@@ -307,14 +505,14 @@ int main()
     scanf("%d", &num);
 
     node arr[num];
-    int ad_mat[num][num], visi[num], in_deg[num];
+    int ad_mat[num][num], visited[num], in_deg[num];
     queue *head = NULL;
 
     cns_graph(num, arr, ad_mat);
 
     calc_in_deg(num, ad_mat, in_deg);
 
-    topo_sort(num, arr, ad_mat, visi, in_deg, head);
+    topo_sort(num, arr, ad_mat, visited, in_deg, head);
 
     // printf("\n\t\t\t::::PROGRAM EXITED::::\n\n");
 
